c92cd8be7cbdb7ff11c55d1db1b60ac2ff858580
kevini78 <133727543+kevini78@users.noreply.github.com>
2023-08-16 21:10:10 -0300

    Add files via upload

---
+++ tarefa3.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+int main(){
+    int num1;
+    int num2;
+    int resultadoMultiplicacao;
+    printf("Digite o valor da base ");
+    scanf("%d",&num1);
+    printf("Digite o valor da altura ");
+    scanf("%d",&num2);
+    resultadoMultiplicacao = num1 * num2;
+    printf(" A area da sala é %d ", resultadoMultiplicacao);
+    
+
+
+
+}
